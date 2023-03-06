b6eb3d23a276a88f04d15992333f496c14020105
Егор Житников <zhitnikoveg@yandex.ru>
2023-03-06 20:35:51 +0300

    test

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+lisq=%4ulbmmse^s4t2fm!vtm^69gxs&hav0b%4n_-1()2g@_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.10.21']
+ALLOWED_HOSTS = ['*']
 
 
 AUTH_USER_MODEL = 'users.User'
