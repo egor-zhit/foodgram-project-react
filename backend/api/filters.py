2c470583c2d4135a7168d4fd560a5b2d20d1bf90
Егор Житников <zhitnikoveg@yandex.ru>
2023-03-10 19:37:27 +0300

    deploy

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -10,19 +10,30 @@ User = get_user_model()
 class RecipeFilter(FilterSet):
     """Фильтер для рецептов."""
 
-    is_favorited = filters.BooleanFilter(field_name='is_favorited')
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     author = filters.ModelChoiceField(queryset=User.objects.all())
     is_in_shopping_cart = filters.BooleanFilter(
-        field_name='is_in_shopping_cart'
+        method='filter_is_in_shopping_cart'
     )
     tags = filters.ModelMultipleChoiceFilter(
         field_name="tags__slug", queryset=TagModel.objects.all(),
         to_field_name="slug"
     )
 
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value == 1:
+            return queryset.filter(favorites__user=user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(shopping_carts__user=self.request.user)
+        return queryset
+
     class Meta:
         model = RecipesModel
-        fields = ["tags", "author", "is_favorited", "is_in_shopping_cart"]
+        fields = ["tags", "author"]
 
 
 class IngredientSearchFilter(SearchFilter):
