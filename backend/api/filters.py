b9787e95312da1f86e9642863ec2abc9a5280904
Егор Житников <zhitnikoveg@yandex.ru>
2023-03-11 22:49:24 +0300

    deploy

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from django_filters import filters
-from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework import FilterSet, filters
 from recipes.models import RecipesModel, TagModel
 from rest_framework.filters import SearchFilter
 
@@ -10,8 +9,10 @@ User = get_user_model()
 class RecipeFilter(FilterSet):
     """Фильтер для рецептов."""
 
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    author = filters.ModelChoiceField(queryset=User.objects.all())
+    is_favorited = filters.BooleanFilter(
+        field_name='is_favorited', method='filter_is_favorited'
+    )
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
     )
@@ -21,9 +22,8 @@ class RecipeFilter(FilterSet):
     )
 
     def filter_is_favorited(self, queryset, name, value):
-        user = self.request.user
-        if value == 1:
-            return queryset.filter(favorites__user=user)
+        if value and self.request.user.is_authenticated:
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
